69d83df70eb4ef2619f1ed9537133207df5a724e
Reginaldo Junior <you@example.com>
2017-11-09 19:45:24 -0200

    adicionando mongodb

--- index.js
+++ index.js
@@ -2,12 +2,36 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var MongoClient = require('mongodb').MongoClient,
+    collection = null,
+    test = require('assert');
+
+var lastId = false;
+var interval = setInterval(function() {
+  MongoClient.connect('mongodb://user:pass@ds011715.mlab.com:11715/tosky', function(err, db) {
+    collection = db.collection('coupons');
+    var cursor = collection.find().limit(1).sort({ $natural : -1 });
+
+    cursor.toArray(function(err, results) {
+      if (err) throw err;
+      console.log(lastId);
+      console.log(results[0]['_id']);
+      // io.emit('order notification', 'teste 123');
+      if (lastId !== results[0]['_id']) {
+        console.log('fa');
+
+        lastId = results[0]['_id'];
+      }
+    });
+  });
+}, 1000);
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/teste', function(req, res){
-  io.emit('chat message', 'teste 123');
+  io.emit('order notification', 'teste 123');
   res.send('done');
 });
 
@@ -18,13 +42,9 @@ io.on('connection', function(socket){
     console.log('user disconnected');
   });
 
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('order notification', function(msg){
+    io.emit('order notification', msg);
   });
-
-  setTimeout(function(){
-    io.emit('chat message', 'teste 123'); 
-  }, 5);
 });
 
 http.listen(3000, function(){
