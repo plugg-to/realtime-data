7ba6fccadf94ce09e663716cc220c5cc54cf3808
Reginaldo Junior <76regi@gmail.com>
2017-11-11 18:28:51 -0200

    backend ok

--- index.js
+++ index.js
@@ -1,47 +1,102 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
-var MongoClient = require('mongodb').MongoClient,
-    test = require('assert');
-
+var request = require('request');
+var MongoClient = require('mongodb').MongoClient;
 var lastId = false;
+var products = null;
+var orders = null;
+var order = false;
+
+async function orderCallBack(err, orderResult) {
+	let address = orderResult[0]['receiver_zipcode'] + ',' + orderResult[0]['receiver_country'];
+	let maps = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=AIzaSyCGpPbK8_SAfu1_cjadJO0u62_C7_2rVuE';
+	
+	request(
+		{
+	    	url: maps,
+	    	json: true
+	  	}, 
+	  	function (error, response, body) {
+			if (String(lastId) !== String(orderResult[0]['_id'])) {
+				order = {
+					'id': orderResult[0]['_id'],
+					'lat': body['results'][0]['geometry']['location']['lat'],
+					'lng': body['results'][0]['geometry']['location']['lng'],
+					'total': orderResult[0]['total'],
+					'items': []
+				}
+
+				if ('sku' in orderResult[0]['items'][0]) {
+					products.find({'sku': orderResult[0]['items'][0]['sku'], 'user_id': orderResult[0]['user_id']}).limit(1).toArray(function(err, results) {
+						if (results == undefined) {
+							console.log(results);
+							return;
+						}
+
+						photos = [];
+
+						
+						try {
+							photos = results[0]['photos'];
+						} catch (error) {
+							photos = [];
+						}
+
+					    order.items.push(
+					    	{
+					    		'name': orderResult[0]['items'][0]['name'],
+					    		'photos': photos
+					    	}
+					    );
+
+						if (String(lastId) !== String(orderResult[0]['_id'])) {
+							console.log(order);
+
+							io.emit('order notification', order);
+
+							lastId = orderResult[0]['_id'];
+						}
+					});
+				}
+			}  
+		}
+	);
+};
+
 var interval = setInterval(function() {
-  MongoClient.connect('mongodb://user:pass@host:port/db', function(err, db) {
-    collection = db.collection('coupons');
-    var cursor = collection.find().limit(1).sort({ $natural : -1 });
+  MongoClient.connect('', function(err, db) {
+    orders = db.collection('orders');
+    products = db.collection('products');
 
-    cursor.toArray(function(err, results) {
-      if (String(lastId) !== String(results[0]['_id'])) {
-        io.emit('order notification', results[0]['_id']);
+    cursor = orders.find().limit(1).sort({ $natural : -1 });
 
-        lastId = results[0]['_id'];
-      }
-    });
+    cursor.toArray(orderCallBack);
   });
 }, 1000);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/teste', function(req, res){
-  io.emit('order notification', 'teste 123');
-  res.send('done');
+	io.emit('order notification', 'teste 123');
+	
+	res.send('done');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+	console.log('a user connected');
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+	});
 
-  socket.on('order notification', function(msg){
-    io.emit('order notification', msg);
-  });
+	socket.on('order notification', function(msg){
+		io.emit('order notification', msg);
+	});
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+	console.log('listening on *:3000');
 });
