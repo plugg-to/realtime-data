60546ab12aee0f451d00c3eb5cfd8ceb0ed8e906
Reginaldo Junior <you@example.com>
2017-11-17 18:57:33 -0200

    fix to works real time data

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var MongoClient = require('mongodb').MongoClient;
 var request 	= require('request');
 var numeral 	= require('numeral');
-var http 		= require('http').Server(app);
 var app		    = require('express')();
+var http 		= require('http').Server(app);
 var io 			= require('socket.io')(http);
 
 var constMongoDB = '';
@@ -12,82 +12,69 @@ var lastId 		 = false;
 var orders 		 = null;
 var order 		 = false;
 
-async function orderCallBack(err, orderResult) {
+function orderCallBack(err, orderResult) {
 	let address = orderResult[0]['receiver_zipcode'] + ',' + orderResult[0]['receiver_country'];
 	let maps = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + keyMaps;
-	
+
 	if (String(lastId) !== String(orderResult[0]['_id'])) {
+		lastId = orderResult[0]['_id'];
+
 		request(
 			{
 		    	url: maps,
 		    	json: true
 		  	}, 
 		  	function (error, response, body) {
-		  		console.log(body);
-				if (String(lastId) !== String(orderResult[0]['_id'])) {
-					try {
-						order = {
-							'id': orderResult[0]['_id'],
-							'lat': body['results'][0]['geometry']['location']['lat'],
-							'lng': body['results'][0]['geometry']['location']['lng'],
-							'total': orderResult[0]['total'],
-							'channel': orderResult[0]['channel'],
-							'items': []
-						};
-					} catch (error) {
-						return;
-					}
+				console.log(body);
 
-					try {
-						products.find({'sku': orderResult[0]['items'][0]['sku'], 'user_id': orderResult[0]['user_id']}).limit(1).toArray(function(err, results) {
-							if (results == undefined) {
-								console.log(results);
-								return;
-							}
+				try {
+					order = {
+						'id': orderResult[0]['_id'],
+						'lat': body['results'][0]['geometry']['location']['lat'],
+						'lng': body['results'][0]['geometry']['location']['lng'],
+						'total': orderResult[0]['total'],
+						'channel': orderResult[0]['channel'],
+						'items': []
+					};
+				} catch (error) {
+					return;
+				}
 
+				try {
+					products.find({'sku': orderResult[0]['items'][0]['sku'], 'user_id': orderResult[0]['user_id']}).limit(1).toArray(function(err, results) {
+						if (results == undefined) {
+							console.log(results);
+							return;
+						}
+
+						photos = [];
+
+						
+						try {
+							photos = results[0]['photos'];
+						} catch (error) {
 							photos = [];
+						}
 
-							
-							try {
-								photos = results[0]['photos'];
-							} catch (error) {
-								photos = [];
-							}
+					    order.items.push(
+					    	{
+					    		'name': orderResult[0]['items'][0]['name'],
+					    		'photos': photos
+					    	}
+					    );
 
-						    order.items.push(
-						    	{
-						    		'name': orderResult[0]['items'][0]['name'],
-						    		'photos': photos
-						    	}
-						    );
-
-							io.emit('order notification', order);
-
-							lastId = orderResult[0]['_id'];
-						});
-					} catch (error) {
 						io.emit('order notification', order);
-
-						lastId = orderResult[0]['_id'];
-					}
-				}  
+					});
+				} catch (error) {
+					io.emit('order notification', order);
+				}
 			}
 		);
 	}
 };
 
-var interval = setInterval(function() {
-  MongoClient.connect(constMongoDB, function(err, db) {
-    orders = db.collection('orders');
-    products = db.collection('products');
-
-    cursor = orders.find({}).limit(1).sort({ $natural : -1 });
-
-    cursor.toArray(orderCallBack);
-  });
-}, 1000);
-
 app.get('/', function(req, res){
+	console.log('hey');
 	res.sendFile(__dirname + '/index.html');
 });
 
@@ -124,6 +111,18 @@ io.on('connection', function(socket){
 	});
 });
 
-http.listen(3000, function(){
-	console.log('listening on *:3000');
+http.listen(3000, () => {
+	console.log('Listening local port 3000');
 });
+
+//for testing, we're just going to send data to the client every second
+setInterval(() => {
+  MongoClient.connect(constMongoDB, function(err, db) {
+    orders = db.collection('orders');
+    products = db.collection('products');
+
+    cursor = orders.find({}).limit(1).sort({ $natural : -1 });
+
+    cursor.toArray(orderCallBack);
+  });
+}, 1000);
